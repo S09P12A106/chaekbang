15313760557bd3f70d553c62d709e0ad18bf92c5
eslee0608 <eslee0608@naver.com>
2023-07-18 14:40:42 +0900

    feat: global style 추가

---
+++ Frontend/src/constants/colors.js
@@ -0,0 +1,15 @@
+
+const COLORS = {
+    // 0이 가장 연합니다. figma 참고
+  THEME_COLOR0: "#cbf1f5",
+  THEME_COLOR1: "#a6e3e9",
+  THEME_COLOR2: "#00bbc6",
+  THEME_COLOR3: "#58b6b6",
+  THEME_COLOR4: "#1f8e94",
+
+  BLACK: "#202020",
+  GREY: "#7B7B7B",
+  WHITE: "#ffffff",
+};
+
+export default COLORS;
---
+++ Frontend/src/styles/GlobalStyle.jsx
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+import colors from './constants/colors';
+
+const GlobalStyle = createGlobalStyle`
+	
+  // 폰트 순차적으로 선택 > 추후 폰트 바꾸겠습니다.
+	* {
+    font-family: 'Pretendard', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
+  // 리스트의 동그라미 지운다.
+  ul, ol {
+    list-style: none;
+  }
+
+  // 링크에 밑줄 없앤다. link에도 적용됨
+  a {
+    color: inherit;
+    text-decoration: none
+  }
+
+  // 링크 및 버튼에 마우스 올리면 손가락 모양
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+
+  // 전체 컨테이너 가로 넓이
+  .container {
+    width: 1340px; 
+    padding-left: 30px; 
+    padding-right: 30px; 
+  }
+
+`;
+
+export default GlobalStyle;
